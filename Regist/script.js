0e740f3fa61553f43295740d76fa9147e6eee4ba
redha_ <redhadefinto28@gmail.com>
2024-12-16 22:23:34 +0700

    change method regist with localStorage

--- Regist/script.js
+++ Regist/script.js
@@ -34,8 +34,8 @@ function cekPassword() {
   const confirmPasswordValue = confirmPassword.value;
   // reset to default
   passwordErrorMessage.textContent = "";
-  inputPassword.classList.remove("input-error");
-  confirmPassword.classList.remove("input-error");
+  inputPassword.classList.remove("input-error", "input-valid");
+  confirmPassword.classList.remove("input-error", "input-valid");
   let isValid = true;
   if (passwordValue === "" || confirmPasswordValue === "") {
     passwordErrorMessage.textContent = "password cannot be empty!";
@@ -43,6 +43,14 @@ function cekPassword() {
     confirmPassword.classList.add("input-error");
     return (isValid = false);
   }
+  console.log(passwordValue.length);
+  if (passwordValue.length < 4 || confirmPasswordValue.length < 4) {
+    passwordErrorMessage.textContent = "Password minimum 4 characters";
+    inputPassword.classList.add("input-error");
+    confirmPassword.classList.add("input-error");
+    return (isValid = false);
+  }
+
   if (passwordValue !== confirmPasswordValue) {
     passwordErrorMessage.textContent = "Password Not Match";
     inputPassword.classList.add("input-error");
@@ -58,15 +66,44 @@ function cekForm(e) {
   e.preventDefault();
   const isMailValid = cekMail();
   const isPasswordValid = cekPassword();
-
+  const formData = new FormData(form);
+  const emailValue = formData.get("email");
+  const passwordValue = formData.get("password");
+  // Mendapatkan data yang sudah ada di localStorage (jika ada) kalau tidak ada mendapatkan array kosong
   if (isMailValid && isPasswordValid) {
-    loader.classList.remove("display-invisible"); // Pastikan loader ditampilkan
     btnRegist.classList.add("display-invisible");
-    loader.classList.add("display-visible");
-    toast.classList.remove("display-invisible");
-    toast.classList.add("display-visible");
+    loader.classList.remove("display-invisible");
+    loader.classList.add("d-block");
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+    const userExists = users.some(
+      (existingUser) => existingUser.email === emailValue
+    );
+    if (userExists) {
+      setTimeout(() => {
+        toast.textContent = "email already exists!";
+        btnRegist.classList.remove("display-invisible");
+        inputMail.classList.remove("input-valid");
+        inputMail.classList.add("input-error");
+        toast.classList.add("toast-error", "display-visible");
+        loader.classList.add("display-invisible");
+      }, 3000);
+      setTimeout(() => {
+        toast.classList.remove("toast-error", "display-visible");
+      }, 5000);
+      return;
+    }
+    const user = {
+      email: emailValue,
+      password: passwordValue,
+    };
+    users.push(user);
+    // Menyimpan array users ke dalam localStorage
+    localStorage.setItem("users", JSON.stringify(users));
+
+    toast.textContent = "Sign Up Success";
+    toast.classList.add("toast-succes", "display-visible");
     setTimeout(() => {
-      location.href = "../EnterPin/index.html";
+      location.href = "../Login/login.html";
     }, 5000);
     return;
   }
